9e1e0dad50c1ad3358f23da93dd5302ae55d4207
gononono64 <96976330+gononono64@users.noreply.github.com>
2025-01-31 13:39:32 -0600

    kablam

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,15 +5,18 @@ description 'mrc-template'
 version '1.0.0'
 
 shared_scripts {
+	'init.lua',
 	'shared/*.lua',
 }
 
 server_scripts {
     'server/*.lua',
+	'init.lua'
 }
 
 client_scripts {
     'client/*.lua',
+	'init.lua'
 }
 
 ui_page "html/index.html"
@@ -22,6 +25,8 @@ files {
 	'html/index.html',
 	'html/script.js',
 	'html/style.css',
+	'shared/*.lua',
+	'client/*.lua',
 }
 
 data_file 'DLC_ITYP_REQUEST' 'stream/mrc-cave.ytyp'
---
+++ init.lua
@@ -0,0 +1,64 @@
+local loadedModules = {}
+
+function Require(modulePath, resourceName)
+    if resourceName and type(resourceName) ~= "string" then
+        resourceName = GetCurrentResourceName()
+    end
+
+    if not resourceName then
+        resourceName = "community_lib"
+    end
+
+    local id = resourceName .. ":" .. modulePath
+    if loadedModules[id] then
+        print("Returning cached module [" .. id .. "]")
+        return loadedModules[id]
+    end
+
+    local file = LoadResourceFile(resourceName, modulePath)
+    if not file then
+        error("Error loading file [" .. id .. "]")
+    end
+
+    local chunk, loadErr = load(file, id)
+    if not chunk then
+        error("Error wrapping module [" .. id .. "] Message: " .. loadErr)
+    end
+
+    local success, result = pcall(chunk)
+    if not success then
+        error("Error executing module [" .. id .. "] Message: " .. result)
+    end
+    loadedModules[id] = result
+    return result
+end
+
+cLib = {
+    Require = Require,
+    Callback = Callback or Require("shared/callbacks.lua"),
+    Ids = Ids or Require("shared/ids.lua"),
+    ReboundEntities = ReboundEntities or Require("shared/rebound_entities.lua"),
+    Tables = Tables or Require("shared/tables.lua"),
+    Math = Math or Require("shared/math.lua"),
+    LA = LA or Require("shared/la.lua"),
+    Perlin = Perlin or Require("shared/perlin.lua"),
+}
+
+exports('cLib', cLib)
+
+if not IsDuplicityVersion() then goto client end
+
+cLib.SQL = SQL or Require("server/sqlHandler.lua")
+cLib.Logs = Logs or Require("server/logs.lua")
+cLib.LootTables = LootTables or Require("server/lootTables.lua")
+
+if IsDuplicityVersion() then return cLib end
+::client::
+
+cLib.Gizmo = Gizmo or Require("client/gizmo.lua")
+cLib.Scaleform = Scaleform or Require("client/scaleform.lua")
+cLib.Utility = Utility or Require("client/utility.lua")
+cLib.PlaceableObject = PlaceableObject or Require("client/object_placer.lua")
+
+
+return cLib
